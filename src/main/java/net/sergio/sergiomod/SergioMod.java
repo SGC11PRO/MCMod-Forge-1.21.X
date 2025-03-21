4d68914c5db1335850bd863f314ff1ded3dbb6c1
Sergio <sergio.garcia.cristobal@gmail.com>
2025-03-21 19:25:25 +0100

    Add first item (lithium ingot)

--- src/main/java/net/sergio/sergiomod/SergioMod.java
+++ src/main/java/net/sergio/sergiomod/SergioMod.java
@@ -1,10 +1,12 @@
 package net.sergio.sergiomod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -13,14 +15,15 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.sergio.sergiomod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(SergioMod.MODID)
+@Mod(SergioMod.MOD_ID)
 public class SergioMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "sergiomod";
+    public static final String MOD_ID = "sergiomod";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -33,6 +36,8 @@ public class SergioMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        // Llama a la funcion register de ModItems
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -49,7 +54,7 @@ public class SergioMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) event.accept(ModItems.LITIO); // Si se abre la pestaña de INGREDIENTES del modo creativo, se registra allí el item LITIO
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -60,7 +65,7 @@ public class SergioMod
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
---
+++ src/main/java/net/sergio/sergiomod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.sergio.sergiomod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sergio.sergiomod.SergioMod;
+
+public class ModItems {
+
+    // crear una nueva lista de ITEMS en SergioMod
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SergioMod.MOD_ID);
+
+    // Registrar un nuevo item LITIO en la lista ITEMS, con el nombre "litio" y las propiedades por defecto Item.Properties()
+    public static final RegistryObject<Item> LITIO = ITEMS.register("litio", () ->
+            new Item(new Item.Properties()));
+
+
+    // Registrar esta lista en Forge
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
